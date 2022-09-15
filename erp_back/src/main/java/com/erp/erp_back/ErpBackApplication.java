162847a48bab6db0c84a23bcf4d8cbf9dd1b12ac
stephane.bohler <steph.bohler@gmail.com>
2022-09-15 13:46:55 +0200

    Add swagger with config

--- erp_back/src/main/java/com/erp/erp_back/ErpBackApplication.java
+++ erp_back/src/main/java/com/erp/erp_back/ErpBackApplication.java
@@ -2,8 +2,10 @@ package com.erp.erp_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ErpBackApplication {
 
 	public static void main(String[] args) {
--- erp_back/src/main/java/com/erp/erp_back/controller/LotController.java
+++ erp_back/src/main/java/com/erp/erp_back/controller/LotController.java
@@ -2,9 +2,12 @@ package com.erp.erp_back.controller;
 
 import com.erp.erp_back.model.Lot;
 import com.erp.erp_back.repository.LotRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api("API pour les opérations CRUD sur les Lots")
 @RestController
 @CrossOrigin
 public class LotController {
@@ -12,7 +15,7 @@ public class LotController {
     @Autowired
     private LotRepository lotRepository;
 
-
+    @ApiOperation(value = "Récupère TOUT les lots existant")
     @RequestMapping(value = "/lots", method = RequestMethod.GET)
     public Iterable<Lot> getAllLots() throws Exception {
         try {
@@ -24,6 +27,7 @@ public class LotController {
         return null;
     }
 
+    @ApiOperation(value = "Récupère UN lot grâce à son ID")
     @RequestMapping({"/lots/{id}"})
     @ResponseBody
     public Lot getLotById(@PathVariable int id) { return this.lotRepository.findById(id); }
