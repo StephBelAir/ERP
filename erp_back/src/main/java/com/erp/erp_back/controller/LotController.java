014b73b3b7508d08dde71be455d6a857456ce982
stephane.bohler <steph.bohler@gmail.com>
2022-09-30 15:05:15 +0200

    Add Patch lot startDate

--- erp_back/src/main/java/com/erp/erp_back/controller/LotController.java
+++ erp_back/src/main/java/com/erp/erp_back/controller/LotController.java
@@ -8,7 +8,13 @@ import com.erp.erp_back.service.LotService;
 import com.erp.erp_back.service.PlanningUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.time.LocalDateTime;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -191,4 +197,18 @@ public class LotController {
     }
     return null;
   }
+
+  @ApiOperation(value = "Mise en production d'UN lot")
+  @PatchMapping("/lot/patch")
+  public ResponseEntity<Lot> updateEmployeePartially(@RequestParam int id, @RequestParam("start")
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate) {
+    try {
+      Lot lot = lotRepository.findById(id);
+      lot.setStartDate(startDate);
+      return new ResponseEntity<Lot>(lotRepository.save(lot), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
 }
