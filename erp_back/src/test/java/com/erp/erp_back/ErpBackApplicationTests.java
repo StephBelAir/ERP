2657c7f97d0bb26af49f56ddfc77c766a5f1804b
stephane.bohler <steph.bohler@gmail.com>
2022-10-04 14:41:01 +0200

    Add my basic First Unit Test

--- erp_back/src/test/java/com/erp/erp_back/ErpBackApplicationTests.java
+++ erp_back/src/test/java/com/erp/erp_back/ErpBackApplicationTests.java
@@ -1,13 +1,33 @@
 package com.erp.erp_back;
 
+import com.erp.erp_back.model.Lot;
+import com.erp.erp_back.repository.LotRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ErpBackApplicationTests {
 
+	@Autowired
+	private LotRepository lotRepository;
+
 	@Test
 	void contextLoads() {
+		// ARRANGE
+		int a = 2;
+		String b = "test";
+		int c = 35;
+		Lot lot = new Lot(a, b, c);
+
+		// ACT
+		lot = lotRepository.save(lot);
+
+		// ASSERT
+		Assertions.assertThat(lot.getId()).isEqualTo(2);
+
+
 	}
 
 }
