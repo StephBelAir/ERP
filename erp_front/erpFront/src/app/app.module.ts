a4d2b830b7ac0cc5927188f87eaa09c8c2383be0
stephane.bohler <steph.bohler@gmail.com>
2022-10-10 13:40:53 +0200

    add pages

--- erp_front/erpFront/src/app/app.module.ts
+++ erp_front/erpFront/src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MachinePageComponent } from './components/machine-page/machine-page.component';
+import { ProcessPageComponent } from './components/process-page/process-page.component';
+import { LotPageComponent } from './components/lot-page/lot-page.component';
+
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MachinePageComponent,
+    ProcessPageComponent,
+    LotPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {path: 'machine-page', component: MachinePageComponent},
+      {path: 'process-page', component: ProcessPageComponent},
+      {path: 'lot-page', component: LotPageComponent},
+      {path: '', redirectTo: '/machine-page', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ]),
     AppRoutingModule,
     BrowserAnimationsModule
   ],
---
+++ erp_front/erpFront/src/app/components/machine-page/machine-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-machine-page',
+  templateUrl: './machine-page.component.html',
+  styleUrls: ['./machine-page.component.scss']
+})
+export class MachinePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
