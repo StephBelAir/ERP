3f41d3eb0748215300b011d8b35a59525e6394aa
stephane.bohler <steph.bohler@gmail.com>
2022-10-12 16:31:31 +0200

    add interfaces in lotService

--- erp_front/erpFront/src/app/services/lot.service.ts
+++ erp_front/erpFront/src/app/services/lot.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {ILot} from "../interfaces/ilot";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,15 @@ export class LotService {
   apiUrl = environment.apiUrl;
 
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient : HttpClient,) { }
 
 
   postLot(lot : any){
     return this.httpClient.post<any>(`${this.apiUrl}/lots/add`,lot)
   }
 
-  getLot(){
-    return this.httpClient.get <any>(`${this.apiUrl}/lots`);
+  getLot(): Observable<ILot[]>{
+    return this.httpClient.get <ILot[]>(`${this.apiUrl}/lots`);
   }
 
 }
