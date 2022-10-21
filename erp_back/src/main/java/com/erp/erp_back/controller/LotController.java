e0f1eca5301d2ae95892f3a6663f09b68a90e114
stephane.bohler <steph.bohler@gmail.com>
2022-10-21 16:22:29 +0200

    Add condition to end a lot only if the lot have a process

--- erp_back/src/main/java/com/erp/erp_back/controller/LotController.java
+++ erp_back/src/main/java/com/erp/erp_back/controller/LotController.java
@@ -257,11 +257,15 @@ public class LotController {
       @RequestBody Lot updateLot) throws Exception {
     try {
       Lot lot = this.lotRepository.findById(updateLot.getId());
-      Process process = lot.getProcess();
+      if (lot.getProcess() != null) {
+        Process process = lot.getProcess();
       lot.setProcess(process);
       lot.setActualEndDate(updateLot.getActualEndDate());
       lotRepository.save(lot);
       return new ResponseEntity<>(lot, HttpStatus.OK);
+      } else {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
     } catch (Exception ex) {
       System.out.println(ex);
     }
